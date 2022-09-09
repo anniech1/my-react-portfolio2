bd847f26c3d97137f2dfca0eac6ff777b66427c8
anniech1 <ach577@nyu.edu>
2022-09-08 20:39:43 -0400

    moved from orginal react portfolio to this one for github pages deploy error

---
+++ src/components/About.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const About =()=>{
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            <br/>
+            Hi, I'm Annie! Coding student from Taiwan, currently based in New York City.
+            
+            <br/>
+            <br className="hidden lg:inline-block" />
+          </h1>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              Contact Me!
+            </a>
+            <br/>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              Click to see examples of previous work!
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About
